6b29e479ef4e8b82e12b97772d140355841598c9
Shubhan Yenuganti <yrshubhan@gmail.com>
2025-03-26 16:27:56 -0700

    Created code to split video into frame per second.

--- detect.py
+++ detect.py
@@ -1,7 +1,14 @@
 from dotenv import load_dotenv
+from split import extract_frames
 import os
 load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
+if __name__ == "__main__": 
+    video_path = "input/video.mp4"
+    frame_dir = "frames"
+
+    # Extract frames
+    extract_frames(video_path, frame_dir)
---
+++ split.py
@@ -0,0 +1,22 @@
+import cv2
+import os
+
+def extract_frames(path, output_dir, interval = 30): 
+    cap = cv2.VideoCapture(path)
+    frame_count = 0
+    saved_count = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        while frame_count % interval == 0:
+            cv2.imwrite(f"{output_dir}/frame_{saved_count}.jpg", frame)
+            saved_count += 1
+        
+        frame_count += 1
+    
+    cap.release()
+
+    print(f"Extracted {saved_count} frames")
